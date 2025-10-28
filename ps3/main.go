76c64c09dbe8c20825f2447bd4d235804de2e4b5
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-10-28 10:44:05 +0000

    add gitignore

--- ps3/main.go
+++ ps3/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    cnp := make(chan func(), 10)
-    for i := 0; i < 4; i++ {
-        go func() {
-            for f := range cnp {
-                f()
-            }
-        }()
-    }
-    cnp <- func() {
-        fmt.Println("HERE1")
-    }
-    fmt.Println("Hello")
+	cnp := make(chan func(), 10)
+	for i := 0; i < 4; i++ {
+		go func() {
+			for f := range cnp {
+				f()
+			}
+		}()
+	}
+	cnp <- func() {
+		fmt.Println("HERE1")
+	}
+	fmt.Println("Hello")
 }
