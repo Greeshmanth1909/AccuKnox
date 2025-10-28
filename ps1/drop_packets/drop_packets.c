01aa86672b0950cee4efe7baad935c070dac14d2
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2025-10-28 10:42:23 +0000

    remove debug messages

--- ps1/drop_packets/drop_packets.c
+++ ps1/drop_packets/drop_packets.c
@@ -28,18 +28,13 @@ int drop_packets(struct xdp_md *ctx) {
         return XDP_PASS;
     }
     
-    bpf_printk("%llu is the port number\n", *portno);
-    
     if ((void*)eth + sizeof(*eth) <= data_end) {
-        bpf_printk("is valid eth\n");
+
         struct iphdr *ip = data + sizeof(*eth);
         if ((void*)ip + sizeof(*ip) <= data_end) {
-            bpf_printk("is valid ip\n");
             if (ip->protocol == IPPROTO_TCP) {
-                bpf_printk("is TCP\n");
                 struct tcphdr *tcp = (void*)ip + sizeof(*ip);
                 if ((void*)tcp + sizeof(*tcp) <= data_end) {
-                    bpf_printk("is valid tcp\n");
                     if (tcp->dest == bpf_htons((__u16)*portno)) {
                         bpf_printk("Dropping packet to port %d\n", bpf_ntohs(tcp->dest));
                         return XDP_DROP;
